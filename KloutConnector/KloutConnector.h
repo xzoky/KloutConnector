6b522038b49ee4c62b74c24e1989da2722e97fa6
Camille Kander <camille@kander.com>
2012-04-30 19:40:13 +0200

    Initial Commit

---
+++ KloutConnector/KloutConnector.h
@@ -0,0 +1,13 @@
+//
+//  KloutConnector.h
+//  KloutConnector
+//
+//  Created by Camille Kander on 30/04/12.
+//  Copyright (c) 2012 Supinfo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KloutConnector : NSObject
+
+@end
